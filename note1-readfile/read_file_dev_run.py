5284ae9f9b6a8c00bfe31198ba03414cab9a98eb
lambert keith <2698730244@qq.com>
2023-10-16 00:07:17 +0800

    1

--- note1-readfile/read_file_dev_run.py
+++ note1-readfile/read_file_dev_run.py
@@ -1,8 +1,20 @@
 from read_file import read_csv_file, read_excel_file, read_json_file, read_txt_file
 
+csv_path = ''
+json_path = 'data/test.json'
+
 
 def read_csv_file_test():
-    read_csv_file
+    '''
+    读取csv文件测试
+    read_flag用来表示读取操作是否成功
+    '''
+    read_flag, data = read_csv_file(filename='data/test.json')
+    if read_flag:
+        # 如果读取成功，可以对数据进行操作
+        print(data[0]['name'])
+    else:
+        print("读取错误，错误如下：", data)
 
 
 
@@ -11,7 +23,7 @@ def read_json_file_test():
     读取json文件测试
     read_flag用来表示读取操作是否成功
     '''
-    read_flag, data = read_json_file(filename='data/test.json')
+    read_flag, data = read_json_file(filename=json_path)
     if read_flag:
         # 如果读取成功，可以对数据进行操作
         print(data[0]['name'])
